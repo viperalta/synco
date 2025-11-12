ac7989648f0ba9792df4b77b3b54542e9772e606
Vicente Peralta <vicente@pharedata.com>
2025-11-12 11:26:18 -0300

    agregada Andre a la lista default

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -457,7 +457,7 @@ const Calendar = () => {
   const LISTA_DEFAULT = [
     'Vicho', 'Cony', 'Lucas', 'Bastian', 'Buri', 'Carlos', 'Diego', 'Catalina', 
     'Gabi', 'Javi Rivas', 'Javi Soto', 'Jorge', 'Kev', 'Kitsu', 'Mariano', 
-    'Romy', 'Claudio andres', 'Sofi', 'Fernando'
+    'Romy', 'Claudio andres', 'Sofi', 'Fernando', 'Andre'
   ];
 
   const ENTRENO = [
